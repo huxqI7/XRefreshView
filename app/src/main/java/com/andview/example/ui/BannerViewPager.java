19ed26faee2fa09a5e4150f458be7c4a6135d7cc
archar <464044128@qq.com>
2021-03-25 16:00:26 +0800

    reset

---
+++ app/src/main/java/com/andview/example/ui/BannerViewPager.java
@@ -0,0 +1,36 @@
+package com.andview.example.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+
+import androidx.viewpager.widget.ViewPager;
+
+/**
+ * Created by huxq17 on 2016/2/1.
+ */
+public class BannerViewPager extends ViewPager {
+    public BannerViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BannerViewPager(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (parent != null) {
+            parent.requestDisallowInterceptTouchEvent(true);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private ViewGroup parent;
+
+    public void setParent(ViewGroup parent) {
+        this.parent = parent;
+    }
+
+}
